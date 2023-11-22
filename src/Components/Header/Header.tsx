0cf6fa27890c57704fa1005880be7a744396daaa
moseung <tmdah900@naver.com>
2023-11-22 20:34:47 +0900

    feat: header 디자인 수정, 기능추가

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -22,13 +22,14 @@ const Header = ({ currentScroll, isHeaderShadow }: HeaderProps) => {
   }
 
   return (
-    <div
+    <header
       style={{
         boxShadow: isHeaderShadow
           ? 'rgba(0, 0, 0, 0.03) 0px 30px 30px 0px'
           : '',
+        backgroundColor: isHeaderShadow ? 'white' : 'transparent',
       }}
-      className=" transition-all duration-300 z-10 w-full bg-white px-24 py-7 flex items-center justify-between fixed top-0"
+      className=" fixed top-0 z-10 flex w-full items-center justify-between px-8 py-5 transition-all duration-300 mobile:px-5"
     >
       <Image
         className=" cursor-pointer rounded-full"
@@ -36,13 +37,14 @@ const Header = ({ currentScroll, isHeaderShadow }: HeaderProps) => {
         height={50}
         src={Moseung}
         alt="Writers logo"
+        onClick={() => window.scrollTo({ behavior: 'smooth', top: 0 })}
       />
       <div className=" flex gap-5">
         {TABS.map((tab, index: number) => (
           <p
             className={`${
               currentScroll === tab.title ? 'text-sub' : ''
-            } cursor-pointer text-4xl font-bold transition-all hover:text-sub`}
+            } cursor-pointer text-2xl font-bold transition-all hover:text-sub mobile:text-xl`}
             key={index}
             onClick={() => handleClickHeader(tab.title)}
           >
@@ -50,7 +52,7 @@ const Header = ({ currentScroll, isHeaderShadow }: HeaderProps) => {
           </p>
         ))}
       </div>
-    </div>
+    </header>
   )
 }
 
