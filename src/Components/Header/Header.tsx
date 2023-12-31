b023a9285aa859591c4be93269549812e2996978
endmoseung <endmoseung>
2023-12-31 15:10:23 +0900

    feat: Header 모바일 구현

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -5,7 +5,7 @@ import React, { useState } from 'react'
 import Moseung from '/public/assets/Images/moseung.jpeg'
 import Tab from './Tab/Tab'
 import useDevice from '@/utils/Hooks/useDevice'
-import { FaBars } from 'react-icons/fa'
+import { FaBars, FaTimes } from 'react-icons/fa'
 
 interface HeaderProps {
   currentScroll: string
@@ -21,12 +21,11 @@ const Header = ({ currentScroll, isHeaderShadow }: HeaderProps) => {
         boxShadow: isHeaderShadow
           ? 'rgba(0, 0, 0, 0.03) 0px 30px 30px 0px'
           : '',
-        backgroundColor: isHeaderShadow ? 'white' : 'transparent',
       }}
-      className=" fixed top-0 z-10 flex w-full items-center justify-between px-8 py-5 transition-all duration-300 mobile:px-5"
+      className="fixed top-0 z-10 flex w-full items-center justify-between bg-white px-8 py-5 transition-all duration-300 mobile:px-5"
     >
       <Image
-        className=" cursor-pointer rounded-full"
+        className="cursor-pointer rounded-full "
         width={50}
         height={50}
         src={Moseung}
@@ -34,15 +33,22 @@ const Header = ({ currentScroll, isHeaderShadow }: HeaderProps) => {
         onClick={() => window.scrollTo({ behavior: 'smooth', top: 0 })}
       />
       {mobile ? (
-        <FaBars
-          className="h-6 w-6 cursor-pointer"
-          onClick={() => setIsTabVisible(!isTabVisible)}
-        />
+        isTabVisible ? (
+          <FaTimes
+            className="h-6 w-6 cursor-pointer"
+            onClick={() => setIsTabVisible(false)}
+          />
+        ) : (
+          <FaBars
+            className="h-6 w-6 cursor-pointer"
+            onClick={() => setIsTabVisible(!isTabVisible)}
+          />
+        )
       ) : (
         <Tab currentScroll={currentScroll} />
       )}
       {isTabVisible && (
-        <div className="absolute bottom-0">
+        <div className="absolute bottom-0 left-0 w-full translate-y-full bg-white px-4 py-5">
           <Tab currentScroll={currentScroll} />
         </div>
       )}
